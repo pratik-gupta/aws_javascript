e00d07ad90342b92c7818aaefb838dadaf2b4991
Pratik Gupta <Pratik.Gupta@pearson.com>
2018-04-26 15:46:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-1'});
+
+// Create S3 service object
+s3_list = new AWS.S3({apiVersion: '2006-03-01'});
+
+// call S3 to list the buckets
+s3_list.listBuckets(function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Buckets);
+  }
+});
